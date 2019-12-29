489204b060f192a4810fe2b2cc13cd6263f03b58
roblikescake <roblik3scak3@gmail.com>
2019-12-29 15:30:54 -0500

    Create UserSettings

---
+++ UserControls/UserSettings.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Forms;
+
+namespace Arma3FishersLifeCraftingGuide.UserControls
+{
+    public partial class UserSettings : UserControl
+    {
+        private static UserSettings _instance;
+        public static UserSettings Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new UserSettings();
+                }
+                return _instance;
+            }
+        }
+        public UserSettings()
+        {
+            InitializeComponent();
+        }
+    }
+}
