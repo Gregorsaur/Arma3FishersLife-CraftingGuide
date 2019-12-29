0137ac66ba1210733f0f1e8b6308ee774786cdcb
roblikescake <roblik3scak3@gmail.com>
2019-12-29 15:30:40 -0500

    Create HelpPage

---
+++ UserControls/HelpPage.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Forms;
+
+namespace Arma3FishersLifeCraftingGuide.UserControls
+{
+    public partial class HelpPage : UserControl
+    {
+        private static HelpPage _instance;
+        public static HelpPage Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new HelpPage();
+                }
+                return _instance;
+            }
+        }
+        public HelpPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
