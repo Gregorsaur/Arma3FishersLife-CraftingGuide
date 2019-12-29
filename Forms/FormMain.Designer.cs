9d43dd711e0cde5a17e15e4a8d5c2eb76b832aa9
roblikescake <roblik3scak3@gmail.com>
2019-12-28 23:41:35 -0500

    Add project files.

---
+++ Forms/FormMain.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace Arma3FishersLifeCraftingGuide.Forms
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.staticPanelHeader = new System.Windows.Forms.Panel();
+            this.staticLabelHeader2 = new System.Windows.Forms.Label();
+            this.staticLabelHeader1 = new System.Windows.Forms.Label();
+            this.staticPanelFooter = new System.Windows.Forms.Panel();
+            this.staticLabelFooter1 = new System.Windows.Forms.Label();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.panelNavigation = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonNavInfo = new System.Windows.Forms.Button();
+            this.buttonNavOther = new System.Windows.Forms.Button();
+            this.buttonNavWeaponsAmmo = new System.Windows.Forms.Button();
+            this.buttonNavFoodDrink = new System.Windows.Forms.Button();
+            this.buttonNavVehicleParts = new System.Windows.Forms.Button();
+            this.buttonNavWatercraft = new System.Windows.Forms.Button();
+            this.buttonNavAircraft = new System.Windows.Forms.Button();
+            this.buttonNavVehicles = new System.Windows.Forms.Button();
+            this.buttonNavHome = new System.Windows.Forms.Button();
+            this.staticHeaderLogo = new System.Windows.Forms.PictureBox();
+            this.staticPanelHeader.SuspendLayout();
+            this.staticPanelFooter.SuspendLayout();
+            this.panelNavigation.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.staticHeaderLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // staticPanelHeader
+            // 
+            this.staticPanelHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(49)))));
+            this.staticPanelHeader.Controls.Add(this.staticHeaderLogo);
+            this.staticPanelHeader.Controls.Add(this.staticLabelHeader2);
+            this.staticPanelHeader.Controls.Add(this.staticLabelHeader1);
+            this.staticPanelHeader.Location = new System.Drawing.Point(8, 8);
+            this.staticPanelHeader.Name = "staticPanelHeader";
+            this.staticPanelHeader.Size = new System.Drawing.Size(1216, 113);
+            this.staticPanelHeader.TabIndex = 24;
+            // 
+            // staticLabelHeader2
+            // 
+            this.staticLabelHeader2.AutoSize = true;
+            this.staticLabelHeader2.Font = new System.Drawing.Font("Times New Roman", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.staticLabelHeader2.ForeColor = System.Drawing.Color.White;
+            this.staticLabelHeader2.Location = new System.Drawing.Point(120, 60);
+            this.staticLabelHeader2.Name = "staticLabelHeader2";
+            this.staticLabelHeader2.Size = new System.Drawing.Size(261, 42);
+            this.staticLabelHeader2.TabIndex = 11;
+            this.staticLabelHeader2.Text = "Crafting Guide";
+            // 
+            // staticLabelHeader1
+            // 
+            this.staticLabelHeader1.AutoSize = true;
+            this.staticLabelHeader1.Font = new System.Drawing.Font("Times New Roman", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.staticLabelHeader1.ForeColor = System.Drawing.Color.White;
+            this.staticLabelHeader1.Location = new System.Drawing.Point(120, 12);
+            this.staticLabelHeader1.Name = "staticLabelHeader1";
+            this.staticLabelHeader1.Size = new System.Drawing.Size(343, 42);
+            this.staticLabelHeader1.TabIndex = 10;
+            this.staticLabelHeader1.Text = "Arma 3 Fisher\'s Life";
+            // 
+            // staticPanelFooter
+            // 
+            this.staticPanelFooter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(49)))));
+            this.staticPanelFooter.Controls.Add(this.staticLabelFooter1);
+            this.staticPanelFooter.Location = new System.Drawing.Point(8, 656);
+            this.staticPanelFooter.Name = "staticPanelFooter";
+            this.staticPanelFooter.Size = new System.Drawing.Size(1216, 47);
+            this.staticPanelFooter.TabIndex = 25;
+            // 
+            // staticLabelFooter1
+            // 
+            this.staticLabelFooter1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.staticLabelFooter1.ForeColor = System.Drawing.Color.Transparent;
+            this.staticLabelFooter1.Location = new System.Drawing.Point(840, 8);
+            this.staticLabelFooter1.Name = "staticLabelFooter1";
+            this.staticLabelFooter1.Size = new System.Drawing.Size(372, 34);
+            this.staticLabelFooter1.TabIndex = 0;
+            this.staticLabelFooter1.Text = "Arma 3 Fisher\'s Life Crafting Guide\r\nBy Richard Axel and Bobby Axel | v0.01 | 201" +
+    "9";
+            this.staticLabelFooter1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // panelMain
+            // 
+            this.panelMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.panelMain.Location = new System.Drawing.Point(160, 128);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(1064, 520);
+            this.panelMain.TabIndex = 29;
+            // 
+            // panelNavigation
+            // 
+            this.panelNavigation.Controls.Add(this.panel1);
+            this.panelNavigation.Location = new System.Drawing.Point(8, 128);
+            this.panelNavigation.Name = "panelNavigation";
+            this.panelNavigation.Size = new System.Drawing.Size(152, 520);
+            this.panelNavigation.TabIndex = 30;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(49)))));
+            this.panel1.Controls.Add(this.buttonNavInfo);
+            this.panel1.Controls.Add(this.buttonNavOther);
+            this.panel1.Controls.Add(this.buttonNavWeaponsAmmo);
+            this.panel1.Controls.Add(this.buttonNavFoodDrink);
+            this.panel1.Controls.Add(this.buttonNavVehicleParts);
+            this.panel1.Controls.Add(this.buttonNavWatercraft);
+            this.panel1.Controls.Add(this.buttonNavAircraft);
+            this.panel1.Controls.Add(this.buttonNavVehicles);
+            this.panel1.Controls.Add(this.buttonNavHome);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(152, 520);
+            this.panel1.TabIndex = 3;
+            // 
+            // buttonNavInfo
+            // 
+            this.buttonNavInfo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonNavInfo.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonNavInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonNavInfo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.buttonNavInfo.Location = new System.Drawing.Point(0, 464);
+            this.buttonNavInfo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonNavInfo.Name = "buttonNavInfo";
+            this.buttonNavInfo.Size = new System.Drawing.Size(152, 58);
+            this.buttonNavInfo.TabIndex = 8;
+            this.buttonNavInfo.Text = "Info";
+            this.buttonNavInfo.UseVisualStyleBackColor = true;
+            // 
+            // buttonNavOther
+            // 
+            this.buttonNavOther.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonNavOther.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonNavOther.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonNavOther.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.buttonNavOther.Location = new System.Drawing.Point(0, 406);
+            this.buttonNavOther.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonNavOther.Name = "buttonNavOther";
+            this.buttonNavOther.Size = new System.Drawing.Size(152, 58);
+            this.buttonNavOther.TabIndex = 7;
+            this.buttonNavOther.Text = "Other";
+            this.buttonNavOther.UseVisualStyleBackColor = true;
+            // 
+            // buttonNavWeaponsAmmo
+            // 
+            this.buttonNavWeaponsAmmo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonNavWeaponsAmmo.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonNavWeaponsAmmo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonNavWeaponsAmmo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.buttonNavWeaponsAmmo.Location = new System.Drawing.Point(0, 348);
+            this.buttonNavWeaponsAmmo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonNavWeaponsAmmo.Name = "buttonNavWeaponsAmmo";
+            this.buttonNavWeaponsAmmo.Size = new System.Drawing.Size(152, 58);
+            this.buttonNavWeaponsAmmo.TabIndex = 6;
+            this.buttonNavWeaponsAmmo.Text = "Weapons/Ammo";
+            this.buttonNavWeaponsAmmo.UseVisualStyleBackColor = true;
+            // 
+            // buttonNavFoodDrink
+            // 
+            this.buttonNavFoodDrink.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonNavFoodDrink.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonNavFoodDrink.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonNavFoodDrink.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.buttonNavFoodDrink.Location = new System.Drawing.Point(0, 290);
+            this.buttonNavFoodDrink.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonNavFoodDrink.Name = "buttonNavFoodDrink";
+            this.buttonNavFoodDrink.Size = new System.Drawing.Size(152, 58);
+            this.buttonNavFoodDrink.TabIndex = 5;
+            this.buttonNavFoodDrink.Text = "Food/Drink";
+            this.buttonNavFoodDrink.UseVisualStyleBackColor = true;
+            // 
+            // buttonNavVehicleParts
+            // 
+            this.buttonNavVehicleParts.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonNavVehicleParts.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonNavVehicleParts.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonNavVehicleParts.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.buttonNavVehicleParts.Location = new System.Drawing.Point(0, 232);
+            this.buttonNavVehicleParts.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonNavVehicleParts.Name = "buttonNavVehicleParts";
+            this.buttonNavVehicleParts.Size = new System.Drawing.Size(152, 58);
+            this.buttonNavVehicleParts.TabIndex = 4;
+            this.buttonNavVehicleParts.Text = "Vehicle Parts";
+            this.buttonNavVehicleParts.UseVisualStyleBackColor = true;
+            // 
+            // buttonNavWatercraft
+            // 
+            this.buttonNavWatercraft.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonNavWatercraft.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonNavWatercraft.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonNavWatercraft.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.buttonNavWatercraft.Location = new System.Drawing.Point(0, 174);
+            this.buttonNavWatercraft.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonNavWatercraft.Name = "buttonNavWatercraft";
+            this.buttonNavWatercraft.Size = new System.Drawing.Size(152, 58);
+            this.buttonNavWatercraft.TabIndex = 3;
+            this.buttonNavWatercraft.Text = "Watercraft";
+            this.buttonNavWatercraft.UseVisualStyleBackColor = true;
+            // 
+            // buttonNavAircraft
+            // 
+            this.buttonNavAircraft.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonNavAircraft.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonNavAircraft.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonNavAircraft.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.buttonNavAircraft.Location = new System.Drawing.Point(0, 116);
+            this.buttonNavAircraft.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonNavAircraft.Name = "buttonNavAircraft";
+            this.buttonNavAircraft.Size = new System.Drawing.Size(152, 58);
+            this.buttonNavAircraft.TabIndex = 2;
+            this.buttonNavAircraft.Text = "Aircraft";
+            this.buttonNavAircraft.UseVisualStyleBackColor = true;
+            // 
+            // buttonNavVehicles
+            // 
+            this.buttonNavVehicles.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonNavVehicles.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonNavVehicles.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonNavVehicles.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.buttonNavVehicles.Location = new System.Drawing.Point(0, 58);
+            this.buttonNavVehicles.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonNavVehicles.Name = "buttonNavVehicles";
+            this.buttonNavVehicles.Size = new System.Drawing.Size(152, 58);
+            this.buttonNavVehicles.TabIndex = 1;
+            this.buttonNavVehicles.Text = "Vehicles";
+            this.buttonNavVehicles.UseVisualStyleBackColor = true;
+            // 
+            // buttonNavHome
+            // 
+            this.buttonNavHome.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonNavHome.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonNavHome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonNavHome.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.buttonNavHome.Location = new System.Drawing.Point(0, 0);
+            this.buttonNavHome.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonNavHome.Name = "buttonNavHome";
+            this.buttonNavHome.Size = new System.Drawing.Size(152, 58);
+            this.buttonNavHome.TabIndex = 0;
+            this.buttonNavHome.Text = "Home";
+            this.buttonNavHome.UseVisualStyleBackColor = true;
+            // 
+            // staticHeaderLogo
+            // 
+            this.staticHeaderLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.staticHeaderLogo.Image = ((System.Drawing.Image)(resources.GetObject("staticHeaderLogo.Image")));
+            this.staticHeaderLogo.Location = new System.Drawing.Point(12, 12);
+            this.staticHeaderLogo.Name = "staticHeaderLogo";
+            this.staticHeaderLogo.Size = new System.Drawing.Size(95, 93);
+            this.staticHeaderLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.staticHeaderLogo.TabIndex = 9;
+            this.staticHeaderLogo.TabStop = false;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(70)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(1233, 714);
+            this.Controls.Add(this.panelNavigation);
+            this.Controls.Add(this.panelMain);
+            this.Controls.Add(this.staticPanelHeader);
+            this.Controls.Add(this.staticPanelFooter);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Arma 3 Fisher\'s Life - Crafting Guide";
+            this.staticPanelHeader.ResumeLayout(false);
+            this.staticPanelHeader.PerformLayout();
+            this.staticPanelFooter.ResumeLayout(false);
+            this.panelNavigation.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.staticHeaderLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel staticPanelHeader;
+        private System.Windows.Forms.PictureBox staticHeaderLogo;
+        private System.Windows.Forms.Label staticLabelHeader2;
+        private System.Windows.Forms.Label staticLabelHeader1;
+        private System.Windows.Forms.Panel staticPanelFooter;
+        private System.Windows.Forms.Label staticLabelFooter1;
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.Panel panelNavigation;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonNavInfo;
+        private System.Windows.Forms.Button buttonNavOther;
+        private System.Windows.Forms.Button buttonNavWeaponsAmmo;
+        private System.Windows.Forms.Button buttonNavFoodDrink;
+        private System.Windows.Forms.Button buttonNavVehicleParts;
+        private System.Windows.Forms.Button buttonNavWatercraft;
+        private System.Windows.Forms.Button buttonNavAircraft;
+        private System.Windows.Forms.Button buttonNavVehicles;
+        private System.Windows.Forms.Button buttonNavHome;
+    }
+}
---
+++ Forms/FormMain.cs
@@ -0,0 +1,152 @@
+﻿using Arma3FishersLifeCraftingGuide.UserControls;
+using System;
+using System.Windows.Forms;
+
+namespace Arma3FishersLifeCraftingGuide.Forms
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            //ShowCraftingHub();
+        }
+
+        private void ShowCraftingHub()
+        {
+            //Hide any panels currently shown
+            HideCraftingPanels();
+
+            //Create panel object, then show on main panel, then bring to front
+            CraftingHub panelCraftingHub = new CraftingHub();
+            panelMain.Controls.Add(panelCraftingHub);
+            panelCraftingHub.BringToFront();
+        }
+
+        private void ShowCraftingVehicle()
+        {
+            //Hide any panels currently shown
+            HideCraftingPanels();
+
+            //Create panel object, then show on main panel, then bring to front
+            CraftingVehicle panelCraftingVehicle = new CraftingVehicle();
+            panelMain.Controls.Add(panelCraftingVehicle);
+            panelCraftingVehicle.BringToFront();
+        }
+
+        private void ShowCraftingAircraft()
+        {
+            //Hide any panels currently shown
+            HideCraftingPanels();
+
+            //Create panel object, then show on main panel
+            CraftingAircraft panelCraftingAircraft = new CraftingAircraft();
+            panelMain.Controls.Add(panelCraftingAircraft);
+        }
+
+        private void ShowCraftingWatercraft()
+        {
+            //Hide any panels currently shown
+            HideCraftingPanels();
+
+            //Create panel object, then show on main panel
+            CraftingWatercraft panelCraftingWatercraft = new CraftingWatercraft();
+            panelMain.Controls.Add(panelCraftingWatercraft);
+        }
+
+        private void ShowCraftingVehicleParts()
+        {
+            //Hide any panels currently shown
+            HideCraftingPanels();
+
+            //Create panel object, then show on main panel
+            CraftingVehicleParts panelCraftingVehicleParts = new CraftingVehicleParts();
+            panelMain.Controls.Add(panelCraftingVehicleParts);
+        }
+
+        private void ShowCraftingFoodDrink()
+        {
+            //Hide any panels currently shown
+            HideCraftingPanels();
+
+            //Create panel object, then show on main panel
+            CraftingFoodDrink panelCraftingFoodDrink = new CraftingFoodDrink();
+            panelMain.Controls.Add(panelCraftingFoodDrink);
+        }
+
+        private void ShowCraftingWeaponsAmmo()
+        {
+            //Hide any panels currently shown
+            HideCraftingPanels();
+
+            //Create panel object, then show on main panel
+            CraftingWeaponsAmmo panelCraftingWeaponsAmmo = new CraftingWeaponsAmmo();
+            panelMain.Controls.Add(panelCraftingWeaponsAmmo);
+        }
+
+        private void ShowCraftingOther()
+        {
+            //Hide any panels currently shown
+            HideCraftingPanels();
+
+            //Create panel object, then show on main panel
+            CraftingOther panelCraftingOther = new CraftingOther();
+            panelMain.Controls.Add(panelCraftingOther);
+        }
+
+        private void HideCraftingPanels()
+        {
+            //Iterate thru main panel and hide all existing panels
+            //Do this before showing new panel
+            foreach (Control ctrl in panelMain.Controls)
+            {
+                ctrl.Visible = false;
+            }
+        }
+
+        private void ButtonNavHome_Click(object sender, EventArgs e)
+        {
+            ShowCraftingHub();
+        }
+
+        private void ButtonNavVehicles_Click(object sender, EventArgs e)
+        {
+            ShowCraftingVehicle();
+        }
+
+        private void ButtonNavAircraft_Click(object sender, EventArgs e)
+        {
+            ShowCraftingAircraft();
+        }
+
+        private void ButtonNavWatercraft_Click(object sender, EventArgs e)
+        {
+            ShowCraftingWatercraft();
+        }
+
+        private void ButtonNavVehicleParts_Click(object sender, EventArgs e)
+        {
+            ShowCraftingVehicleParts();
+        }
+
+        private void ButtonNavFoodDrink_Click(object sender, EventArgs e)
+        {
+            ShowCraftingFoodDrink();
+        }
+
+        private void ButtonNavWeaponsAmmo_Click(object sender, EventArgs e)
+        {
+            ShowCraftingWeaponsAmmo();
+        }
+
+        private void ButtonNavOther_Click(object sender, EventArgs e)
+        {
+            ShowCraftingOther();
+        }
+
+        private void ButtonNavInfo_Click(object sender, EventArgs e)
+        {
+            //todo
+        }
+    }
+}
