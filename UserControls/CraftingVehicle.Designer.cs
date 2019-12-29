edab472823a0e5e7ef2ae1cd646939c4ad698d38
roblikescake <roblik3scak3@gmail.com>
2019-12-29 13:28:41 -0500

    Wrong colour, oops

--- UserControls/CraftingVehicle.Designer.cs
+++ UserControls/CraftingVehicle.Designer.cs
@@ -45,6 +45,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(70)))), ((int)(((byte)(73)))));
             this.Controls.Add(this.label1);
             this.Name = "CraftingVehicle";
             this.Size = new System.Drawing.Size(1064, 520);
