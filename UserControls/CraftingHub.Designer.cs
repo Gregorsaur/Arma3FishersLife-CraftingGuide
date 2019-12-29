653af90c5b29583c4d3da2c96949087dda8e5278
roblikescake <roblik3scak3@gmail.com>
2019-12-29 01:15:43 -0500

    Fixing some things

--- UserControls/CraftingHub.Designer.cs
+++ UserControls/CraftingHub.Designer.cs
@@ -28,172 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panelCraftingOptions = new System.Windows.Forms.Panel();
-            this.buttonCraftVehicle = new System.Windows.Forms.Button();
-            this.buttonCraftAircraft = new System.Windows.Forms.Button();
-            this.buttonCraftOther = new System.Windows.Forms.Button();
-            this.buttonCraftWatercraft = new System.Windows.Forms.Button();
-            this.buttonCraftWeaponsAmmo = new System.Windows.Forms.Button();
-            this.buttonCraftVehicleParts = new System.Windows.Forms.Button();
-            this.buttonCraftFoodDrink = new System.Windows.Forms.Button();
-            this.panelChooseCraft = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.panelCraftingOptions.SuspendLayout();
-            this.panelChooseCraft.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // panelCraftingOptions
-            // 
-            this.panelCraftingOptions.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(49)))));
-            this.panelCraftingOptions.Controls.Add(this.buttonCraftVehicle);
-            this.panelCraftingOptions.Controls.Add(this.buttonCraftAircraft);
-            this.panelCraftingOptions.Controls.Add(this.buttonCraftOther);
-            this.panelCraftingOptions.Controls.Add(this.buttonCraftWatercraft);
-            this.panelCraftingOptions.Controls.Add(this.buttonCraftWeaponsAmmo);
-            this.panelCraftingOptions.Controls.Add(this.buttonCraftVehicleParts);
-            this.panelCraftingOptions.Controls.Add(this.buttonCraftFoodDrink);
-            this.panelCraftingOptions.Location = new System.Drawing.Point(96, 280);
-            this.panelCraftingOptions.Name = "panelCraftingOptions";
-            this.panelCraftingOptions.Size = new System.Drawing.Size(880, 224);
-            this.panelCraftingOptions.TabIndex = 27;
-            // 
-            // buttonCraftVehicle
-            // 
-            this.buttonCraftVehicle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(69)))), ((int)(((byte)(71)))));
-            this.buttonCraftVehicle.FlatAppearance.BorderSize = 0;
-            this.buttonCraftVehicle.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.buttonCraftVehicle.ForeColor = System.Drawing.Color.LimeGreen;
-            this.buttonCraftVehicle.Location = new System.Drawing.Point(24, 24);
-            this.buttonCraftVehicle.Name = "buttonCraftVehicle";
-            this.buttonCraftVehicle.Size = new System.Drawing.Size(180, 75);
-            this.buttonCraftVehicle.TabIndex = 14;
-            this.buttonCraftVehicle.Text = "Vehicle";
-            this.buttonCraftVehicle.UseVisualStyleBackColor = false;
-            // 
-            // buttonCraftAircraft
-            // 
-            this.buttonCraftAircraft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(69)))), ((int)(((byte)(71)))));
-            this.buttonCraftAircraft.FlatAppearance.BorderSize = 0;
-            this.buttonCraftAircraft.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.buttonCraftAircraft.ForeColor = System.Drawing.Color.LimeGreen;
-            this.buttonCraftAircraft.Location = new System.Drawing.Point(240, 24);
-            this.buttonCraftAircraft.Name = "buttonCraftAircraft";
-            this.buttonCraftAircraft.Size = new System.Drawing.Size(180, 75);
-            this.buttonCraftAircraft.TabIndex = 15;
-            this.buttonCraftAircraft.Text = "Aircraft";
-            this.buttonCraftAircraft.UseVisualStyleBackColor = false;
-            // 
-            // buttonCraftOther
-            // 
-            this.buttonCraftOther.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(69)))), ((int)(((byte)(71)))));
-            this.buttonCraftOther.FlatAppearance.BorderSize = 0;
-            this.buttonCraftOther.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.buttonCraftOther.ForeColor = System.Drawing.Color.LimeGreen;
-            this.buttonCraftOther.Location = new System.Drawing.Point(560, 128);
-            this.buttonCraftOther.Name = "buttonCraftOther";
-            this.buttonCraftOther.Size = new System.Drawing.Size(180, 75);
-            this.buttonCraftOther.TabIndex = 20;
-            this.buttonCraftOther.Text = "Other";
-            this.buttonCraftOther.UseVisualStyleBackColor = false;
-            // 
-            // buttonCraftWatercraft
-            // 
-            this.buttonCraftWatercraft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(69)))), ((int)(((byte)(71)))));
-            this.buttonCraftWatercraft.FlatAppearance.BorderSize = 0;
-            this.buttonCraftWatercraft.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.buttonCraftWatercraft.ForeColor = System.Drawing.Color.LimeGreen;
-            this.buttonCraftWatercraft.Location = new System.Drawing.Point(456, 24);
-            this.buttonCraftWatercraft.Name = "buttonCraftWatercraft";
-            this.buttonCraftWatercraft.Size = new System.Drawing.Size(180, 75);
-            this.buttonCraftWatercraft.TabIndex = 16;
-            this.buttonCraftWatercraft.Text = "Watercraft";
-            this.buttonCraftWatercraft.UseVisualStyleBackColor = false;
-            // 
-            // buttonCraftWeaponsAmmo
-            // 
-            this.buttonCraftWeaponsAmmo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(69)))), ((int)(((byte)(71)))));
-            this.buttonCraftWeaponsAmmo.FlatAppearance.BorderSize = 0;
-            this.buttonCraftWeaponsAmmo.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.buttonCraftWeaponsAmmo.ForeColor = System.Drawing.Color.LimeGreen;
-            this.buttonCraftWeaponsAmmo.Location = new System.Drawing.Point(344, 128);
-            this.buttonCraftWeaponsAmmo.Name = "buttonCraftWeaponsAmmo";
-            this.buttonCraftWeaponsAmmo.Size = new System.Drawing.Size(180, 75);
-            this.buttonCraftWeaponsAmmo.TabIndex = 19;
-            this.buttonCraftWeaponsAmmo.Text = "Weapons/Ammo";
-            this.buttonCraftWeaponsAmmo.UseVisualStyleBackColor = false;
-            // 
-            // buttonCraftVehicleParts
-            // 
-            this.buttonCraftVehicleParts.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(69)))), ((int)(((byte)(71)))));
-            this.buttonCraftVehicleParts.FlatAppearance.BorderSize = 0;
-            this.buttonCraftVehicleParts.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.buttonCraftVehicleParts.ForeColor = System.Drawing.Color.LimeGreen;
-            this.buttonCraftVehicleParts.Location = new System.Drawing.Point(672, 24);
-            this.buttonCraftVehicleParts.Name = "buttonCraftVehicleParts";
-            this.buttonCraftVehicleParts.Size = new System.Drawing.Size(180, 75);
-            this.buttonCraftVehicleParts.TabIndex = 17;
-            this.buttonCraftVehicleParts.Text = "Vehicle Parts";
-            this.buttonCraftVehicleParts.UseVisualStyleBackColor = false;
-            // 
-            // buttonCraftFoodDrink
-            // 
-            this.buttonCraftFoodDrink.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(69)))), ((int)(((byte)(71)))));
-            this.buttonCraftFoodDrink.FlatAppearance.BorderSize = 0;
-            this.buttonCraftFoodDrink.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.buttonCraftFoodDrink.ForeColor = System.Drawing.Color.LimeGreen;
-            this.buttonCraftFoodDrink.Location = new System.Drawing.Point(128, 128);
-            this.buttonCraftFoodDrink.Name = "buttonCraftFoodDrink";
-            this.buttonCraftFoodDrink.Size = new System.Drawing.Size(180, 75);
-            this.buttonCraftFoodDrink.TabIndex = 18;
-            this.buttonCraftFoodDrink.Text = "Food/Drink";
-            this.buttonCraftFoodDrink.UseVisualStyleBackColor = false;
-            // 
-            // panelChooseCraft
-            // 
-            this.panelChooseCraft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(49)))));
-            this.panelChooseCraft.Controls.Add(this.label1);
-            this.panelChooseCraft.Location = new System.Drawing.Point(96, 16);
-            this.panelChooseCraft.Name = "panelChooseCraft";
-            this.panelChooseCraft.Size = new System.Drawing.Size(880, 224);
-            this.panelChooseCraft.TabIndex = 28;
             // 
             // label1
             // 
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F);
-            this.label1.Location = new System.Drawing.Point(8, 8);
+            this.label1.Font = new System.Drawing.Font("Agency FB", 55F);
+            this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(864, 208);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "What would you like to craft?";
+            this.label1.Size = new System.Drawing.Size(1064, 520);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Welcome to the\r\nCrafting Guide!\r\n\r\nTo get started, choose what you would like to " +
+    "craft via the navigation bar on the left.";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // CraftingHub
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(70)))), ((int)(((byte)(73)))));
-            this.Controls.Add(this.panelChooseCraft);
-            this.Controls.Add(this.panelCraftingOptions);
+            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "CraftingHub";
             this.Size = new System.Drawing.Size(1064, 520);
-            this.panelCraftingOptions.ResumeLayout(false);
-            this.panelChooseCraft.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panelCraftingOptions;
-        private System.Windows.Forms.Button buttonCraftVehicle;
-        private System.Windows.Forms.Button buttonCraftAircraft;
-        private System.Windows.Forms.Button buttonCraftOther;
-        private System.Windows.Forms.Button buttonCraftWatercraft;
-        private System.Windows.Forms.Button buttonCraftWeaponsAmmo;
-        private System.Windows.Forms.Button buttonCraftVehicleParts;
-        private System.Windows.Forms.Button buttonCraftFoodDrink;
-        private System.Windows.Forms.Panel panelChooseCraft;
         private System.Windows.Forms.Label label1;
     }
 }
